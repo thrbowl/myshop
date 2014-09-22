565329ee39db63903f5a8476c29b4f6fafea2d0d
Mao Yunlong <b43258@freescale.com>
2014-09-22 18:21:57 +0800

    add validate for order

--- static/js/order.js
+++ static/js/order.js
@@ -1,4 +1,25 @@
 ;
 $(function () {
-
+    // 订单表单验证
+    $.formValidator.initConfig({
+        formID: "orderForm",
+        onError: function (msg) {
+            alert(msg)
+        },
+        onSuccess: function () {
+            var $form = $("#" + this.formID);
+            $.post($form.attr("action"), $form.serialize(), function (message) {
+                $.message(message);
+                if (message.type == "success") {
+                    $.facebox.close();
+                }
+            });
+        }
+    });
+    $("#name").formValidator({onShow: "请填写收货人姓名", onFocus: "", onCorrect: "", defaultValue: ""})
+        .regexValidator({regExp: "", dataType: "enum", onError: "收货人姓名不能为空"});
+    $("#mobile").formValidator({onShow: "请填写收货人手机号码", onFocus: "", onCorrect: "", defaultValue: ""})
+        .regexValidator({regExp: "", dataType: "enum", onError: "收货人手机号码不能为空"});
+    $("#address").formValidator({onShow: "请填写收货地址", onFocus: "", onCorrect: "", defaultValue: ""})
+        .regexValidator({regExp: "", dataType: "enum", onError: "收货地址不能为空"});
 });
